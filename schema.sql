3588d186d5ca554da6d9100093abd5f028093ba7
Mikhail Kirillov <wmentor@mail.ru>
2020-07-07 21:51:47 +0300

    add update/delete loc triggers

--- schema.sql
+++ schema.sql
@@ -30,7 +30,7 @@ BEGIN
   IF NEW.parent_id = 0 THEN
     NEW.code = NEW.name;
   ELSE
-    NEW.code = ( SELECT NEW.name || ' / ' || code FROM locs WHERE id = NEW.parent_id LIMIT 1 );
+    NEW.code = ( SELECT NEW.name || '/' || code FROM locs WHERE id = NEW.parent_id LIMIT 1 );
   END IF;
   IF NEW.forms IS NULL THEN
     NEW.forms = NEW.name;
@@ -40,6 +40,27 @@ END;
 $BODY$ LANGUAGE plpgsql;
 
 CREATE TRIGGER on_insert_loc_trg BEFORE INSERT ON locs FOR EACH ROW EXECUTE PROCEDURE on_insert_loc();
+
+CREATE FUNCTION on_update_loc() RETURNS trigger AS $BODY$
+BEGIN
+  IF NEW.name != OLD.name THEN
+    NEW.code = REPLACE(OLD.code, OLD.name, NEW.name);
+    UPDATE locs SET code = REPLACE(code, OLD.code, NEW.code) WHERE code LIKE ( '%/' || OLD.CODE );
+  END IF;
+  RETURN NEW;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+CREATE TRIGGER on_update_loc_trg BEFORE UPDATE ON locs FOR EACH ROW EXECUTE PROCEDURE on_update_loc();
+
+CREATE FUNCTION on_delete_loc() RETURNS trigger AS $BODY$
+BEGIN
+  DELETE FROM locs WHERE code LIKE ( '%/' || OLD.code);
+  RETURN OLD;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+CREATE TRIGGER on_delete_loc_trg BEFORE DELETE ON locs FOR EACH ROW EXECUTE PROCEDURE on_delete_loc();
 
 INSERT INTO locs(name,parent_id,latitude,longitude)
 VALUES ('Россия',0,61.698653, 99.505405);
