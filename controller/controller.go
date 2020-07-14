9e0203831bde552ae39f542fae5fb19bc0f594e7
Mikhail Kirillov <wmentor@mail.ru>
2020-07-14 21:25:28 +0300

    issue #20 export points

--- controller/controller.go
+++ controller/controller.go
@@ -3,4 +3,5 @@ package controller
 import (
 	_ "github.com/wmentor/gencset/controller/export"
 	_ "github.com/wmentor/gencset/controller/locs"
+	_ "github.com/wmentor/gencset/controller/points"
 )
---
+++ controller/points/points.go
@@ -0,0 +1,78 @@
+package points
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/wmentor/gencset/db"
+	"github.com/wmentor/serv"
+)
+
+func init() {
+	serv.Register("GET", "/points", handler)
+}
+
+type Loc struct {
+	Id        int64
+	Code      string
+	Latitude  float64
+	Longitude float64
+}
+
+func handler(c *serv.Context) {
+
+	c.SetContentType("text/plain; charset=utf-8")
+	c.WriteHeader(200)
+
+	id := int64(0)
+
+	maker := bytes.NewBuffer(nil)
+
+	dbh, err := db.Get()
+	if err != nil {
+		panic(err)
+	}
+	defer dbh.Close()
+
+	sth, err := dbh.Prepare("SELECT id, code, latitude, longitude FROM locs WHERE id > $1 AND NOT skip ORDER BY id LIMIT 100")
+	if err != nil {
+		panic(err)
+	}
+	defer sth.Close()
+
+	var l Loc
+
+	for {
+
+		maker.Reset()
+
+		rows, err := sth.Query(id)
+		if err != nil {
+			panic(err)
+		}
+
+		cnt := 0
+
+		for rows.Next() {
+			if err := rows.Scan(&l.Id, &l.Code, &l.Latitude, &l.Longitude); err != nil {
+				panic(err)
+			}
+
+			id = l.Id
+
+			code := strings.ToLower(l.Code)
+			maker.WriteString(code)
+			maker.WriteRune(';')
+			maker.WriteString(fmt.Sprintf("%.9f;%.9f\n", l.Latitude, l.Longitude))
+
+			cnt++
+		}
+
+		c.Write(maker.Bytes())
+
+		if cnt < 100 {
+			break
+		}
+	}
+}
